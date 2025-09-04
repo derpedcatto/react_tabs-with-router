8350d5fd222faaf22946e3f33f4c2c9b786c969e
Oleksandr Vannovskyi <alexandervannovskiy@gmail.com>
2025-09-04 21:29:31 +0300

    possible fix for requirements

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
 
-const getNavLinkClass = ({ isActive }: { isActive: boolean }) =>
-  classNames('navbar-item', {
-    'is-active': isActive,
-  });
-
 export const NavBar = () => {
   return (
     <nav
@@ -13,14 +8,29 @@ export const NavBar = () => {
       data-cy="Nav"
     >
       <div className="container">
-        <div className="navbar-brand">
-          <NavLink to="/" className={getNavLinkClass}>
-            Home
-          </NavLink>
-          <NavLink to="/tabs" className={getNavLinkClass}>
-            Tabs
-          </NavLink>
-        </div>
+        <ul className="navbar-brand">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                classNames('navbar-item', { 'is-active': isActive })
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/tabs"
+              className={({ isActive }) =>
+                classNames('navbar-item', { 'is-active': isActive })
+              }
+            >
+              Tabs
+            </NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   );
